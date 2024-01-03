4664a53ea859da644acc6b042a4d31029aed7dc9
shashi <shashi.kamle99@gmail.com>
2024-01-03 19:09:54 +0530

    added terraform files

---
+++ dev.tfvars
@@ -0,0 +1,7 @@
+client_id = "c0d92253-ea34-4fa1-a768-5daf67b45480"
+
+client_secret = "dose1202ff387a4851c5ec10b7afc199faaa"
+
+tags = "db-test"
+
+region = "us-east-2"
---
+++ provider.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    databricks = {
+      source = "databricks/databricks"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.15.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+// initialize provider in "MWS" mode to provision new workspace
+provider "databricks" {
+  alias         = "mws"
+  host          = "https://accounts.cloud.databricks.com"
+  account_id    = var.databricks_account_id
+  client_id     = var.client_id
+  client_secret = var.client_secret
+}
