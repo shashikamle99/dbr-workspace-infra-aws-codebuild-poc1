99e6eacad4c70ae6ef9dc302a4af34ab9282b462
shashi <shashi.kamle99@gmail.com>
2024-01-05 16:43:39 +0530

    a

--- provider.tf
+++ provider.tf
@@ -12,8 +12,8 @@ terraform {
 
 provider "aws" {
   region = var.region
-  access_key = "my-access-key"
-  secret_key = "my-secret-key"
+  access_key = 
+  secret_key = 
 }
 
 // initialize provider in "MWS" mode to provision new workspace
