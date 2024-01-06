25841519c4e744b6c2e73381c6644291b8a3d05b
shashi <shashi.kamle99@gmail.com>
2024-01-06 23:55:27 +0530

    a

--- cross_account _role.tf
+++ cross_account _role.tf
@@ -22,5 +22,15 @@ resource "databricks_mws_credentials" "this" {
   account_id       = var.databricks_account_id
   role_arn         = aws_iam_role.cross_account_role.arn
   credentials_name = "${local.prefix}-creds"
-  depends_on       = [aws_iam_role_policy.this,aws_iam_role.cross_account_role]
+  #depends_on       = [aws_iam_role_policy.this,aws_iam_role.cross_account_role]
+  depends_on = [
+    time_sleep.wait
+  ]
+}
+
+resource "time_sleep" "wait" {
+  depends_on = [
+    aws_iam_role.cross_account_role
+  ]
+  create_duration = "15s"
 }
