584ef277e93bfe2a1f92cb0af2296b243ff81f06
shashikamle99 <154310776+shashikamle99@users.noreply.github.com>
2024-01-10 11:53:48 +0530

    Update s3_backend.tf

--- s3_backend.tf
+++ s3_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dbr-artifact-1"
-    key    = "db-workspace"
-    region = "us-east-1"
+    key    = "terra-backend-s3"
+    region = "us-east-2"
   }
 }
